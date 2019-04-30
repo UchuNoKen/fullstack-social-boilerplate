2eb02b197ba8b1c3b58a6044a623fa300a3957ea
UchuNoKen <xen.o.fite@gmail.com>
2019-04-30 18:35:21 -0400

    destructuring direct exports

--- app.js
+++ app.js
@@ -7,6 +7,16 @@
 // console.log("TOTAL: ", total);
 
 // Destructuring
-const { sum } = require("./helpers");
+const { sum2, sub2, mult2, div2 } = require("./helpers");
 
-const total = sum(10, 200);
+const total = sum2(10, 200);
+console.log("TOTAL: ", total);
+
+const diff = sub2(200, 75);
+console.log("DIFF: ", diff);
+
+const times = mult2(10, 75);
+console.log("TIMES: ", times);
+
+const div = div2(200, 4);
+console.log("DIV: ", div);
--- helpers.js
+++ helpers.js
@@ -2,4 +2,10 @@
 // module.exports = { sum };
 
 // Direct Export
-exports.sum = (a, b) => a + b;
+exports.sum2 = (a, b) => a + b;
+
+exports.mult2 = (a, b) => a * b;
+
+exports.sub2 = (a, b) => a - b;
+
+exports.div2 = (a, b) => a / b;
